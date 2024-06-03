ae11a06ed2615edf8e3f1442122335721a6bc50c
Cristiano <cristianosilvamolon@gmail.com>
2024-06-03 14:14:33 -0300

    configurando a estrutura

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from "../../components/Titulo"
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/CristianoMolon.png" />
+    <Titulo>Cristiano Molon</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,12 @@
+import Titulo from "../../components/Titulo"
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre mim</Titulo>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, sunt nisi? Vero possimus rem, et expedita, tempora odio est optio facere dicta beatae amet. Assumenda voluptatem facilis molestias impedit quaerat!
+    </p>
+  </section>
+)
+
+export default Sobre
