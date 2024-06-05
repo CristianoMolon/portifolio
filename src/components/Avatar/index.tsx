4a0157113ccad16a2e686460951bf068f1bae925
Cristiano <cristianosilvamolon@gmail.com>
2024-06-05 16:15:10 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/CristianoMolon.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,19 @@
+import Avatar from "../../components/Avatar"
+import Paragrafo from "../../components/Paragrafo"
 import Titulo from "../../components/Titulo"
+
+import { Descricao, Botaotema, SidebarContainer } from './styles'
+
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/CristianoMolon.png" />
-    <Titulo fontSize={20}>Cristiano Molon</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Cristiano Molon</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>CristianoMolon</Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>Engenheiro Front-end</Descricao>
+      <Botaotema>Trocar tema</Botaotema>
+    </SidebarContainer>
   </aside>
 )
 
